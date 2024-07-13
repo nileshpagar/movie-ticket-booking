80cb4f80533f7fc8c53981a2c38738656c94b9f4
nileshpagar <nileshpagar@gmail.com>
2024-07-13 18:34:21 +0800

    fixed CinemaViewTest added ChoiceViewTest

--- src/test/java/io/gic/cinema/ui/ChoiceViewTest.java
+++ src/test/java/io/gic/cinema/ui/ChoiceViewTest.java
@@ -1,7 +1,5 @@
 package io.gic.cinema.ui;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
